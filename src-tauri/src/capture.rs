b9d743a4795ecd3c855483b16ae9e2a5b82f5758
SpikeHD <spikegdofficial@gmail.com>
2022-07-06 21:29:11 -0700

    gave up rendering on the fly, now write screenshot to cache

--- src-tauri/src/capture.rs
+++ src-tauri/src/capture.rs
@@ -1,9 +1,10 @@
-use std::io::Write;
+use std::{io::Write, fs};
+use rand::prelude::*;
 
 use tauri;
 
 use screenshots::Screen;
-use minifb::{Key, Window, WindowOptions};
+// use minifb::{Key, Window, WindowOptions};
 
 fn capture_screenshot() {
   // All screens
@@ -15,59 +16,13 @@ fn capture_screenshot() {
   // Capture entire screen
   let image = screen.capture().unwrap();
 
-  // Convert the buffer to a decoded PNG
-  let image_decode = png::Decoder::new(std::io::BufReader::new(image.buffer().as_ref() as &[u8]));
-  let mut reader = image_decode.read_info().unwrap();
+  // Write buffer to file in .cache (random name)
+  let mut rng = rand::thread_rng();
+  let num: u32 = rng.gen();
 
-  // Create a buffer based on the decoded PNG
-  let mut buf = vec![0; reader.output_buffer_size()];
-
-  // Read the next frame
-  reader.next_frame(&mut buf).unwrap();
-
-  let width = image.width() as usize;
-  let height = image.height() as usize;
-
-  let u32_buffer: Vec<u32> = buf
-    .chunks(4)
-    .map(|v| ((v[0] as u32) << 16) | ((v[1] as u32) << 8) | v[2] as u32)
-    .collect();
-
-  // Create a window
-  let mut window = Window::new(
-    "Capture",
-    width,
-    height,
-    WindowOptions::default()
-  ).unwrap();
-
-  let mut captured = false;
-  let mut has_clicked_down = false;
-  let mut starting_pos = (0, 0);
-  let mut ending_pos = (0, 0);
-
-  while window.is_open() && !window.is_key_down(Key::Escape) && !captured {
-    // Write the image to the window
-    window.update_with_buffer(&u32_buffer, width, height).unwrap();
-
-    // Check if mouse is pressed
-    let clickingDown = window.get_mouse_down(minifb::MouseButton::Left);
-
-    if (clickingDown && !has_clicked_down) {
-      has_clicked_down = true;
-      starting_pos.0 = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().0 as i32;
-      starting_pos.1 = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().1 as i32;
-    }
-
-    if (!clickingDown && has_clicked_down) {
-      ending_pos.0 = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().0 as i32;
-      ending_pos.1 = window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap().1 as i32;
-      captured = true;
-    }
-  }
-
-  println!("starting x: {} y: {}", starting_pos.0, starting_pos.1);
-  println!("ending x: {} y: {}", ending_pos.0, ending_pos.1);
+  let filename = format!("{}.png", num);
+  let path = format!(".cache/{}", filename);
+  fs::write(path, image.buffer());
 }
 
 #[tauri::command]
